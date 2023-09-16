434e55575ebf07765d9e8fe6056b01bb95b24db4
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-16 19:01:07 -0400

    Add false data factory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,23 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Factory\CommentFactory;
+use App\Factory\ProductFactory;
+use App\Factory\TagFactory;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        /* Tags */
+        TagFactory::createMany(5);
 
-        $manager->flush();
+        /* Products */
+        ProductFactory::createMany(20, [
+            'comments' => CommentFactory::new()->many(0, 5),
+            'tags' => TagFactory::randomRange(2, 5)
+        ]);
     }
 }
