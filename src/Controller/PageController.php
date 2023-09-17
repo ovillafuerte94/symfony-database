4fbb69d2ae66d95fb0198232525a273c761d6bca
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-16 22:36:01 -0400

    Change data access from the controller

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Tag;
 use App\Entity\Comment;
 use App\Entity\Product;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\CommentRepository;
+use App\Repository\ProductRepository;
+use App\Repository\TagRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,25 +16,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PageController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, TagRepository $tagRepository, ProductRepository $productRepository): Response
     {
         $tag = null;
         
         if ($request->get('tag')) {
-            $tag = $entityManager->getRepository(Tag::class)->findOneBy(['name' => $request->get('tag')]);
+            $tag = $tagRepository->findOneBy(['name' => $request->get('tag')]);
         }
 
         return $this->render('page/index.html.twig', [
-            'products' => $entityManager->getRepository(Product::class)->findLatest($tag),
+            'products' => $productRepository->findLatest($tag),
         ]);
     }
 
     #[Route('/tag/{id}', name: 'app_tag')]
-    public function tag(Tag $tag, EntityManagerInterface $entityManager): Response
+    public function tag(Tag $tag, ProductRepository $productRepository): Response
     {
         return $this->render('page/tag.html.twig', [
             'tag' => $tag,
-            'products' => $entityManager->getRepository(Product::class)->findByTag($tag)
+            'products' => $productRepository->findByTag($tag)
         ]);
     }
 
@@ -45,10 +47,10 @@ class PageController extends AbstractController
     }
 
     #[Route('/comments', name: 'app_comments')]
-    public function comments(EntityManagerInterface $entityManager): Response
+    public function comments(CommentRepository $commentRepository): Response
     {
         return $this->render('page/comments.html.twig', [
-            'comments' => $entityManager->getRepository(Comment::class)->findAllComments()
+            'comments' => $commentRepository->findAllComments()
         ]);
     }
 }
